574ccba9257debc22f4eaedaf89cf476c192efc9
Nostalogicwh <apocalypsewh@163.com>
2020-09-25 20:06:11 +0800

    leetcode 106

---
+++ m_从中序与后序遍历构造二叉树/106.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        # 实际上inorder 和 postorder一定是同时为空的，因此你无论判断哪个都行
+        if not inorder:
+            return None
+        root = TreeNode(postorder[-1])
+        i = inorder.index(root.val)
+        root.left = self.buildTree(inorder[:i], postorder[:i])
+        root.right = self.buildTree(inorder[i+1:], postorder[i:-1])
+
+        return root
