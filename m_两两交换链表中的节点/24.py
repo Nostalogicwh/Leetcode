cdd5e49fc747efd9d0a7d7e1297fee0ab3523303
Nostalogicwh <apocalypsewh@163.com>
2020-10-13 21:00:32 +0800

    24

---
+++ m_两两交换链表中的节点/24.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        thead = ListNode(-1)
+        thead.next = head
+        c = thead
+        while c.next and c.next.next:
+            a, b=c.next, c.next.next
+            c.next, a.next = b, b.next
+            b.next = a
+            c = c.next.next
+        return thead.next
