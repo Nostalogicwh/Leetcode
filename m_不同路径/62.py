008b4ed6975ced64c2dabfb12e5c2254609d1711
Nostalogicwh <apocalypsewh@163.com>
2020-12-09 15:11:45 +0800

    leetcode 62

---
+++ m_不同路径/62.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        f = [[0]*n for zong in range(m)]
+        for i in range(m):
+            f[i][0] = 1
+        for j in range(n):
+            f[0][j] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                f[i][j] = f[i-1][j]+f[i][j-1]
+        return f[-1][-1]
